a865c60170b2212e2c796805841a384c10a64404
coreywang <coreywang@outlook.com>
2013-06-08 17:28:10 +0800

    完成对村委会评价表导出功能

---
+++ src/test/java/com/rcuinfomanager/service/ExportInfo2VillagerCommittee4EstimationTest.java
@@ -0,0 +1,31 @@
+package com.rcuinfomanager.service;
+
+import com.rcuinfomanager.model.BaseInfo;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExportInfo2VillagerCommittee4EstimationTest {
+
+    @Test
+    public void readTemplateAndExportTest() throws IOException {
+        BaseInfo baseInfo = new BaseInfo();
+        baseInfo.setCustomerName("杨木枝");
+        baseInfo.setAddress("小岞镇新桥村09组1号");
+        baseInfo.setCerNum("350521195611012000");
+        BaseInfo baseInfo1 = new BaseInfo();
+        baseInfo1.setCustomerName("杨伟强");
+        baseInfo1.setAddress("小岞镇新桥村09组2号");
+        baseInfo1.setCerNum("350521197811212000");
+        List<BaseInfo> baseInfoList = new ArrayList<BaseInfo>();
+        baseInfoList.add(baseInfo);
+        baseInfoList.add(baseInfo1);
+        ExportInfo2VillagerCommittee4Estimation exportInfo2VillagerCommittee4Estimation = new ExportInfo2VillagerCommittee4Estimation();
+        MockHttpServletResponse mockHttpServletResponse = new MockHttpServletResponse();
+        exportInfo2VillagerCommittee4Estimation.readTemplateAndExport(baseInfoList,mockHttpServletResponse);
+
+    }
+}
